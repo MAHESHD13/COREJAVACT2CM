2f26d0e50729cace8368470292df97d65deacd56
acer <acer@192.168.0.105>
2022-12-16 12:25:57 +0530

    LAMBDA EXPRESSION

---
+++ MY_PRACTICE/src/svkp/OperatorDemo.java
@@ -0,0 +1,14 @@
+package svkp;
+@FunctionalInterface
+interface Operator{
+  public void operate(int num);
+}
+public class OperatorDemo
+
+{
+	public static void main(String[] args) 
+	{
+		Operator op=num -> System.out.println("Increasing num by 2: "+(num+2));
+        op.operate(10);
+	}
+}
