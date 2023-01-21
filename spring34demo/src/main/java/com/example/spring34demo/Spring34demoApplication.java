62b325c75699daa6220c5eaee5ab933eaefe935b
SVKP <SVKP@192.168.0.101>
2023-01-21 12:36:47 +0530

    SPRING_BOOT

---
+++ spring34demo/src/main/java/com/example/spring34demo/Spring34demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring34demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring34demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring34demoApplication.class, args);
+	}
+
+}
